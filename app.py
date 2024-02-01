54c1f87d121c761198919b50852074e6ce0b4342
Fred <121038233@upq.edu.mx>
2024-02-01 15:24:57 -0600

    Cambios
    Cambios en el back end del login

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']='123456'
+app.config['MYSQL_DB']='' # base de datos proporcionada por Jesus pendiente
+
+mysql=MySQL(app)
 
 # Ruta para mostrar el formulario de inicio de sesión
 @app.route('/login', methods=['GET'])
@@ -13,15 +21,22 @@ def show_login_form():
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
+    
+    #creamos el cursor para trabajar
+    cur = mysql.connection.cursor()
 
-    # Aquí debes implementar la lógica de autenticación
-    # Verifica las credenciales y redirige según el resultado
-    if username == 'usuario' and password == 'contraseña':
-        # Usuario autenticado, redirige a la página principal, por ejemplo
-        return redirect(url_for('home'))
-    else:
-        # Credenciales incorrectas, vuelve a mostrar el formulario de inicio de sesión con un mensaje de error
-        return render_template('login.html', error='Credenciales incorrectas')
+    #Creamos la consulta de la base de datos
+    cur.execute("SELECT * FROM user WHERE username %s AND password =%s",(username, password))
+
+    #Obtnenemos el usuario si existe
+
+    user = cur.fetchone()
+    cur.close()
+
+    #comoprobamos el usuario
+
+    if user:
+        return
 
 # Ruta para la página principal después de iniciar sesión (ejemplo)
 @app.route('/home')
