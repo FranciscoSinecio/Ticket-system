267d2863335ce132cca6541914279974bd0f56d1
Fred <121038233@upq.edu.mx>
2024-02-08 14:52:37 -0600

    Update app.py
    Se modfico el loggin

--- app.py
+++ app.py
@@ -1,47 +1,74 @@
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+# -> configuración de la base de datos
 
-app.config['MYSQL_HOST']= 'localhost'
-app.config['MYSQL_USER']= 'root'
-app.config['MYSQL_PASSWORD']='123456'
-app.config['MYSQL_DB']='' # base de datos proporcionada por Jesus pendiente
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'flask_test'
 
-mysql=MySQL(app)
+mysql = MySQL(app)
 
-# Ruta para mostrar el formulario de inicio de sesión
-@app.route('/login', methods=['GET'])
-def show_login_form():
-    return render_template('login.html')
+#definimos una funcion para el manejo de tupla a diccionario
+#----------------------------------------------------------------------------------------------------
+def dict_factory(cursor,row):
+    d ={}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+        return d
+#----------------------------------------------------------------------------------------------------
+@app.before_request
+def configure_row_factory():
+    mysql.connection.cursor().row_factory = dict_factory
+#----------------------------------------------------------------------------------------------------   
+# ->Vistas
+#----------------------------------------------------------------------------------------------------
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
 
-# Ruta para procesar el formulario de inicio de sesión
-@app.route('/login', methods=['POST'])
+#----------------------------------------------------------------------------------------------------
+@app.route('/panelCliente')
+def panelCliente():
+    if 'logueado' in session and session['logueado']:
+        #conecto a la base de datos para la consulta para la obtencion de la informacion del cliente 
+        cur = mysql.connect.cursor()
+        cur.execute('SELECT * FROM cliente WHERE nombre =%s',(session['id'],))
+        user_info = cur.fetchone()
+        cur.close()
+
+        return render_template('panelcliente.html', user_info=user_info)
+    else:
+        return redirect(url_for('login'))
+
+#----------------------------------------------------------------------------------------------------
+
+#-> Logica de acceso
+
+#----------------------------------------------------------------------------------------------------
+@app.route('/acces-login',methods=["GET","POST"])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    if request.method == 'POST'and 'txtcorreo'in request.form and 'txtpassword':
+        _correo = request.form['txtcorreo']
+        _password = request.form['txtpassword']
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM cliente WHERE correo_electronico =%s AND contrasena = %s', (_correo,_password,))
+        account = cur.fetchone()
+        cur.close()
+        print (account)  # debe de borrarse una vez que haya conectado 
+
+        if account:
+            session['logueado'] = True
+            session['id'] = account[0]
+        return redirect(url_for('panelCliente'))
+    else:
+            return render_template('login.html')
     
-    #creamos el cursor para trabajar
-    cur = mysql.connection.cursor()
+#----------------------------------------------------------------------------------------------------
 
-    #Creamos la consulta de la base de datos
-    cur.execute("SELECT * FROM user WHERE username %s AND password =%s",(username, password))
-
-    #Obtnenemos el usuario si existe
-
-    user = cur.fetchone()
-    cur.close()
-
-    #comoprobamos el usuario
-
-    if user == :
-        return
-
-# Ruta para la página principal después de iniciar sesión (ejemplo)
-@app.route('/home')
-def home():
-    return '¡Bienvenido a la página principal!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#----------------------------------------------------------------------------------------------------
+if __name__ =='__main__':
+    app.run(debug = True, )
